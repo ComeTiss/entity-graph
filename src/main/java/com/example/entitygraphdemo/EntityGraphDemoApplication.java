3b5ad494b433ca689381f757f130f70261497c62
Come Tisserand <cometisserand@rocketmail.com>
2023-03-30 17:49:29 +0200

    feat: boot strap project

---
+++ src/main/java/com/example/entitygraphdemo/EntityGraphDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.entitygraphdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityGraphDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityGraphDemoApplication.class, args);
+	}
+
+}
